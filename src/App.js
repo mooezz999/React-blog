3544e9839011614d1d1cb8ccb9132547386c33fc
Moustafa Ezz <65693055+mooezz999@users.noreply.github.com>
2021-06-01 19:29:13 +0200

    Multiple components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar';
+import Home from './Home';
 function App() {
   const title = 'Welcome to the new blog'
   const likes = 50;
@@ -8,9 +9,9 @@ function App() {
   return (
     <div className="App">
  <div className="content">
-   <h1>{title}</h1>
-   <p>Liked {likes} times</p>
-   <a href={link} target="_blank">click here to visit google</a>
+   <Navbar />
+   <Home />
+   
  </div>
     </div>
   );
