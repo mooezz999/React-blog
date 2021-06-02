68074860a57b978d8fd682e3864c09035c565737
Moustafa Ezz <65693055+mooezz999@users.noreply.github.com>
2021-06-02 22:13:05 +0200

    Making a custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,10 @@
 import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
+import useFetch from './usefetch';
 const Home = () => {
    //  let name='mario';
-const [blogs, setBlogs] = useState(null);
-const [isPending, setIsPending] = useState(true);
-const [error, setError] = useState(null);
-
-   useEffect(() => {
-setTimeout(() =>{
-    fetch('http://localhost:8000/blogs')
-.then(res => {
-     if(!res.ok){
-         throw Error('could not fetch the data for that resource');
-     }
-   return res.json();
-})
-.then(data => {
-    console.log(data);
-    setBlogs(data);
-    setIsPending(false);
-    setError(null);
-})
-.catch(err => {
-setIsPending(false)
-setError(err.message);
-}) 
-}, 1000);
-   }, []);
+const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+  
     return ( 
         <div className="home">
             {error && <div>{error}</div>}
