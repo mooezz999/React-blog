86e06287d9dd794ceae7a10c4a985dc5c3295cdf
Moustafa Ezz <65693055+mooezz999@users.noreply.github.com>
2021-06-04 01:24:36 +0200

    useEffect cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
-  const title = 'Welcome to the new blog'
-  const likes = 50;
-  const link = "https://www.google.com"
+
   return (
     <Router>
     <div className="App">
--- src/usefetch.js
+++ src/usefetch.js
@@ -6,8 +6,10 @@ const [isPending, setIsPending] = useState(true);
 const [error, setError] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController();
+
         setTimeout(() =>{
-            fetch(url)
+            fetch(url, {signal: abortCont.signal})
         .then(res => {
              if(!res.ok){
                  throw Error('could not fetch the data for that resource');
@@ -21,10 +23,15 @@ const [error, setError] = useState(null);
             setError(null);
         })
         .catch(err => {
-        setIsPending(false)
+            if (err.name === 'AbortError'){
+                console.log('fetch aborted');
+            }else{
+        setIsPending(false);
         setError(err.message);
+            }
         }) 
         }, 1000);
+        return() => abortCont.abort();
            }, [url]);
            return{data, isPending, error}
 }
