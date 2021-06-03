bc7997213e2fa4ae23f1e7eb47421fe9093321cb
Moustafa Ezz <65693055+mooezz999@users.noreply.github.com>
2021-06-03 23:34:53 +0200

    The React Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import logo from './logo.svg';
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   const title = 'Welcome to the new blog'
   const likes = 50;
   const link = "https://www.google.com"
   return (
+    <Router>
     <div className="App">
+    <Navbar />
  <div className="content">
-   <Navbar />
-   <Home />
+<Switch>
+  <Route path="/">
+    <Home />
+  </Route>
+</Switch>
    
  </div>
     </div>
+    </Router>
   );
 }
 
